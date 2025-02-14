4e4277dd1925cb0ad0680d979da783703ebf4d12
VitaliyMal <>
2025-02-14 20:16:29 +0300

    14.02_2

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,21 @@
+//using JwtAuthenticationManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddOcelot(builder.Configuration);
+//builder.Services.AddCustomJwtAuthentication(builder.Configuration);
+
+var app = builder.Build();
+app.MapHealthChecks("/healthz");
+
+app.UseAuthentication();
+app.UseAuthorization();
+await app.UseOcelot();
+
+app.Run();
